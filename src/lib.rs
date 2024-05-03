1e4e34f23b8469f753c88e16ba9bb90a835e5515
Gabriel Zanetti <38334108+gabrielztk@users.noreply.github.com>
2024-05-02 22:50:18 -0300

    folder structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
-pub mod set_1_basics;
+mod set_1_basics;
+mod set_2_block_crypto;
+mod set_3_block_and_stream_crypto;
+mod set_4_stream_crypto_and_randomness;
+mod set_5_diffie_hellman_and_friends;
+mod set_6_rsa_and_dsa;
+mod set_7_hashes;
+mod set_8_abstract_algebra;
 pub mod utils;
